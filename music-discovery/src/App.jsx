90913c83513ee0f2f59240b03e3f161043119555
benji_707 <dontjunkthis@gmail.com>
2025-11-10 18:07:28 -0600

    first commit, main branch; REACT dependencies installed, App.jsx, App.css, and index.css cleared to default

---
+++ music-discovery/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  return (
+
+      <div>
+        <h1>Welcome to the Unit 1 Final Project</h1>
+      </div>
+  )
+}
+
+export default App
