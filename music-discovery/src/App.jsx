1532e7fe4e469ac8a4f507398e21f1c4a4eb5424
benji_707 <dontjunkthis@gmail.com>
2025-11-18 02:07:23 -0600

    Milestone 3.2 - HomePage and Header: added first media queries for breakpoints

--- music-discovery/src/App.jsx
+++ music-discovery/src/App.jsx
@@ -15,7 +15,7 @@ function App() {
     <div>
       <Header />
       <main>
-        <PlaylistPage />
+        <SelectSourcePage />
       </main>
       <Footer />
     </div>
--- music-discovery/src/components/MediaPlayer.jsx
+++ music-discovery/src/components/MediaPlayer.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import '../styles/MediaPlayer.css';
+import './styles/MediaPlayer.css';
 
 function MediaPlayer() {
   return (
